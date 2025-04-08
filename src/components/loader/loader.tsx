17be49fc8799ad4442165c5087b66ed099a911f8
ASSASSIN-AUS <tamilthasan11@gamil.com>
2025-04-09 00:18:30 +0530

    fix

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Spinner: React.FC = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/10">
-      <div className="w-12 h-12 border-4 border-white rounded-full shadow-xl border-t-transparent animate-spin"></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-md">
+      <div className="w-12 h-12 border-4 border-white rounded-full shadow-2xl border-t-transparent animate-spin"></div>
     </div>
   );
 };
