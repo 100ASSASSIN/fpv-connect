6b041a1a261ef55855e5b23ff0f409c25b20fbb3
ASSASSIN-AUS <tamilthasan11@gamil.com>
2025-04-09 01:50:04 +0530

    fix

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link';
-import Image from 'next/image';
 
 const navigation = [
   { name: 'Home', href: '/' },
